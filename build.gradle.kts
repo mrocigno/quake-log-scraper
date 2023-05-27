bf4faf49201003d9228d9e2a08a8349ee209ca26
mrocigno <rocignom@gmail.com>
2023-05-26 23:32:38 -0300

    fix environment and setting assets

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,6 @@ repositories {
 }
 
 dependencies {
-//    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     testImplementation(kotlin("test"))
 }
 
@@ -25,6 +24,6 @@ kotlin {
 
 tasks.jar {
     manifest {
-        attributes["Main-Class"] = "_mainKt"
+        attributes["Main-Class"] = "br.com.mrocigno._mainKt"
     }
 }
